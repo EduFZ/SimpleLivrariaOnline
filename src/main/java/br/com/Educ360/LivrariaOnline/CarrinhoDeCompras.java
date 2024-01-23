26014daf68cdeccd3ca886447a31a3deb435e0eb
Eduardo Zimmermann <z.edufz@gmail.com>
2024-01-23 17:17:54 -0300

    Criado instancia da classe CarrinhoDeCompras

--- src/main/java/br/com/Educ360/LivrariaOnline/CarrinhoDeCompras.java
+++ src/main/java/br/com/Educ360/LivrariaOnline/CarrinhoDeCompras.java
@@ -25,6 +25,8 @@ public class CarrinhoDeCompras {
                 '}';
     }
 
-
+//    public double calcularTotal(CarrinhoDeCompras carrinhoDeCompras) {
+//
+//    }
 
 }
--- src/main/java/br/com/Educ360/LivrariaOnline/Main.java
+++ src/main/java/br/com/Educ360/LivrariaOnline/Main.java
@@ -1,12 +1,24 @@
 package br.com.Educ360.LivrariaOnline;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        LivroFisico livroFisico = new LivroFisico("Livro1", "Fulano", "001", 12.0, 1.0, 5.0);
-        double v = livroFisico.precoTotal(livroFisico);
-        System.out.println(livroFisico);
-        System.out.println("Preço Total: " + v);
+        Livro livroF1 = new LivroFisico("Livro1", "Fulano", "001", 12.0, 2.0, 1.0);
+        Livro ebook1 = new Ebook("Livro2", "Fulano de Tal", "002", 10, 20);
+
+
+//        double v = livroF1.precoTotal(livroF1);
+//        System.out.println(livroF1);
+//        System.out.println("Preço Total: " + v);
+
+        ArrayList<Livro> listaCompras = new ArrayList<Livro>();
+        listaCompras.add(livroF1);
+
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras(listaCompras);
+        System.out.println(carrinhoDeCompras);
+
 
     }
 }
